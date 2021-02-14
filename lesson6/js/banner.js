dad13595f327ce3e7c8c6f492faf961d52507782
Eadjei25 <asa20005@byui.edu>
2021-02-14 01:36:08 +0000

    week6

---
+++ lesson6/js/banner.js
@@ -0,0 +1,4 @@
+var day= new Date().getDay();
+	if(day== 6){
+		document.write("<div style='font-size: 22pt; padding: 20px;'>Saturday= Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavillion</div>");
+				}
---
+++ lesson6/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('current-year').innerHTML= new Date().getFullYear();
+
+const months= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], 
+	  days= ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var dateFormatted= days[new Date().getDay()]+ ', ' + new Date().getDate() + ' ' + months[new Date().getMonth()]
++ ' ' + new Date().getFullYear();
+document.getElementById('current-date').textContent= dateFormatted;
+
+
+function navMenu() {
+  document.getElementById('nav-menu').classList.toggle('hidden');
+}
