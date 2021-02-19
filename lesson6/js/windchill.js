661062e01d2b140a858bc1f1fd2ad58064c52908
Eadjei25 <asa20005@byui.edu>
2021-02-19 05:10:53 +0000

    last commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,10 @@
 let tempF= parseFloat(document.getElementById('temperature').innerText);
 let windSpeed= parseFloat(document.getElementById('wind').innerText);
 let windChill= 35.74+0.6215*tempF-35.75*windSpeed**0.16+0.4275*tempF*windSpeed**0.16;
+
 if ((tempF <= 50) && (windSpeed >= 3)) {
 	document.getElementById('chill').innerHTML= Math.round(windChill * 100)/100 + '<p>&deg;F<p>';
 }
-document.getElementById('chill').innerHTML= "N/A";
+else {
+	document.getElementById('chill').innerHTML= "N/A";
+}
